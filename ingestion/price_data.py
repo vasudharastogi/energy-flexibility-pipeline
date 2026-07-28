51e088c6422a49057d1e579207643546c651e7a1
Vasudha Rastogi <vasudhaa.rastogi@ymail.com>
2026-07-28 13:29:58 +0200

    Fix error handling and finish backfill/ingest_latest in price ingestion

--- ingestion/price_data.py
+++ ingestion/price_data.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 
 # This script deals with the price ingestion.
 
+
 FILTER_ID = 4169
 REGION = 'DE'
 RESOLUTION = 'hour'
@@ -20,47 +21,70 @@ def convert_ms_to_date(ms):
     return dt
 
 
-# calls index_{resolution}.json, returns the list of week-start timestamps
- # quarterhour: gaps are 168h apart -> 1 week, each entry is the start timestamp of one week-long chunk
-def get_available_weeks(filter_id, region, resolution):
-    r = requests.get(
-        f'https://www.smard.de/app/chart_data/{filter_id}/{region}/index_{resolution}.json', timeout=10)
-    
-    data = r.json()
-    latest_week = data['timestamps'][-1]
+# calls index_{resolution}.json, returns the list of weekly start timestamps
+def get_available_weeks(filter_id, region, resolution) -> list[int]:
+    try:
+        r = requests.get(
+            f'https://www.smard.de/app/chart_data/{filter_id}/{region}/index_{resolution}.json', timeout=10)
+        r.raise_for_status()
 
+        data = r.json()
+        latest_week = data['timestamps'][-1]
+    except requests.exceptions.HTTPError as err_h:
+        print("HTTP Error")
+        print(err_h)
+        return []
+    except requests.exceptions.ConnectionError as con_err:
+        print("Connection error.")
+        return []
+
+    print(f'Data of total {len(data['timestamps'])} weeks available.')
     print(f'Most recent week {convert_ms_to_date(latest_week)}')
     return data['timestamps']
 
 
-# calls the per-week endpoint, returns a list of {timestamp_utc, price_eur_mwh} records
-def fetch_week_prices(filter_id, region, resolution, week_timestamp_ms):
-    res = requests.get(
-        f'https://www.smard.de/app/chart_data/{filter_id}/{region}/{filter_id}_{region}_{resolution}_{week_timestamp_ms}.json',
-        timeout=10)
-
-    latest_prices = []
-    for value in res.json()['series']:
-        if value[1] is not None:
-            latest_prices.append(PriceReading(
-                convert_ms_to_date(value[0]), value[1]))
+# calls the per-week endpoint, returns a list of daily {timestamp_utc, price_eur_mwh} records
+def fetch_week_prices(filter_id, region, resolution, week_timestamp_ms) -> list[PriceReading]:
+    try:
+        res = requests.get(
+            f'https://www.smard.de/app/chart_data/{filter_id}/{region}/{filter_id}_{region}_{resolution}_{week_timestamp_ms}.json',
+            timeout=10)
+        latest_prices = []
+        for value in res.json()['series']:
+            if value[1] is not None:
+                latest_prices.append(PriceReading(
+                    convert_ms_to_date(value[0]), value[1]))
+    except requests.exceptions.HTTPError as err_h:
+        print("HTTP Error")
+        print(err_h)
+        return []
+    except requests.exceptions.ConnectionError as con_err:
+        print("Connection error.")
+        return []
 
     return latest_prices
 
+
 # walks backward through get_available_weeks(), calls fetch_week_prices for each, collects all records
 def backfill(filter_id, region, resolution, num_weeks: int) -> list[PriceReading]:
-    weeks = get_available_weeks(filter_id, region, resolution)[-num_weeks]
+    all_weeks = get_available_weeks(filter_id, region, resolution)
     all_readings = []
-    for week_ts in weeks:
-        pass
+    if len(all_weeks) > num_weeks:
+        weeks = all_weeks[-num_weeks:]
+        for week_ts in weeks:
+            all_readings.extend(fetch_week_prices(
+                filter_id, region, resolution, week_ts))
+    else:
+        print(
+            f'Requested number of weeks: {num_weeks} exceeds available data of {len(all_weeks)}.')
+    return all_readings
 
 
-def ingest_latest(filter_id, region, resolution) -> list[dict]:
-    # just fetch_week_prices() for the single latest week
-    ...
+# just fetch_week_prices() for the single latest week
+def ingest_latest(filter_id, region, resolution) -> list[PriceReading]:
+    latest_week_ts = get_available_weeks(filter_id, region, resolution)[-1]
+    return fetch_week_prices(filter_id, region, resolution, latest_week_ts)
 
 
-# __name__ == "__main__":
-weekly_data = get_available_weeks(4169, 'DE', 'hour')
-latest_week = weekly_data[-1]
-fetch_week_prices(4169, 'DE', 'hour', latest_week)
+if __name__ == "__main__":
+    print(ingest_latest(FILTER_ID, REGION, RESOLUTION))
