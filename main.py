9c94a7e81e8cfa92731d5489c02787dc16839500
Vasudha Rastogi <vasudhaa.rastogi@ymail.com>
2026-08-23 19:14:05 +0200

    Use backfill() instead of ingest_latest() to pull energy price history

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from ingestion.price_data import ingest_latest
+from ingestion.price_data import backfill
 from ingestion.weather_data import get_air_temperature, get_global_radiation
 from persistence import save_prices, save_weather
 from db import get_connection
@@ -8,15 +8,18 @@ FILTER_ID = 4169
 REGION = 'DE'
 RESOLUTION = 'hour'
 
+# Timeline
+WEEKS = 12
+
 # Weather
 RADIATION_STATIONS_ID = '18044'     # Berlin-Brandenburg
 TEMPERATURE_STATIONS_ID = '00427'   # Berlin-Brandenburg
 
 
 # fetch and ingest data
-prices = ingest_latest(FILTER_ID, REGION, RESOLUTION)
-radiation = get_global_radiation(RADIATION_STATIONS_ID, 12)
-temperature = get_air_temperature(TEMPERATURE_STATIONS_ID, 12)
+prices = backfill(FILTER_ID, REGION, RESOLUTION, WEEKS)
+radiation = get_global_radiation(RADIATION_STATIONS_ID, WEEKS)
+temperature = get_air_temperature(TEMPERATURE_STATIONS_ID, WEEKS)
 
 # persit
 conn = get_connection()
